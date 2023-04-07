e1b7a7b30f71804813f4110681f783a53e62ad65
Leet <programingwithli@gmail.com>
2023-04-07 17:41:11 +0800

    feat: 封装全局提示框组件

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
     <n-notification-provider placement="bottom-right">
       <component :is="layout" />
     </n-notification-provider>
+    <Snackbar />
   </v-app>
 </template>
 
 <script setup>
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+
+import Snackbar from '@/components/Snackbar.vue'
 
 const route = useRoute()
 
