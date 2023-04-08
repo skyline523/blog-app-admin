60628bb2b0aeaad43f5b3b0b1d885f58bbee70a0
Leet <programingwithli@gmail.com>
2023-04-08 18:23:02 +0800

    feat: 首页侧边栏

--- src/layout/default.vue
+++ src/layout/default.vue
@@ -1,20 +1,30 @@
 <template>
   <v-layout>
-    <v-navigation-drawer permanent absolute theme="dark" color="#111827">
-        <v-list>
-          <v-list-item color="white" prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg">
+    <v-navigation-drawer
+      theme="dark"
+      color="#111827"
+      class="px-2"
+      width="280"
+    >
+        <v-list class="pt-4">
+          <v-list-item
+            prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+            title="Admin"
+            subtitle="Leet Programming"
+            class="py-2"
+            to="/"
+            :ripple="false"
+          >
           </v-list-item>
         </v-list>
 
-        <v-divider></v-divider>
-
-        <v-list :lines="false" nav class="py-4">
+        <v-list :lines="false" nav>
           <v-list-item
             v-for="(item, index) in sidebarItem"
             :key="index"
             :value="item.path"
             :to="item.path"
-            active-color="#2196f3"
+            active-class="active-router"
           >
             <template v-slot:prepend>
               <v-icon :icon="item.meta.icon" class="mr-3"></v-icon>
@@ -52,5 +62,22 @@ onMounted(() => {
   width: 100%;
   height: 100%;
   background: #FAFAFA;
+}
+.v-list--nav {
+  .v-list-item {
+    :deep(&__content) {
+      color: #9DA4AE;
+    }
+  }
+}
+.active-router {
+  .v-list-item {
+    :deep(&__prepend) {
+      color: #1565C0;
+    }
+    :deep(&__content) {
+      color: #ffffff;
+    }
+  }
 }
 </style>
